af14b7b996e5a87ff032532dfc1faa50a3b7eaeb
Aastha Gupta <aastha1406@gmail.com>
2020-07-24 22:53:58 +0530

    google oauth completed

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
 const express = require("express");
 const app = express();
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("./config/keys");
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback",
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log(accessToken);
+      console.log(refreshToken);
+      console.log(profile);
+    }
+  )
+);
 
-app.get("/", (req, res) => {
-  res.send({ hi: "there" });
-});
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+  })
+);
+
+app.get("/auth/google/callback", passport.authenticate("google"));
 
 app.listen(process.env.PORT || 5000);
