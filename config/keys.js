429da2a180f057993a1da7f82e2bd1c2422655bc
Aastha Gupta <aastha1406@gmail.com>
2020-08-05 15:04:13 +0530

    bugs fixing

--- config/keys.js
+++
@@ -1,8 +0,0 @@
-//keys.js - logic to return dev or prod credentials
-if (process.env.NODE_ENV === "production") {
-  //prod env
-  module.exports = require("./prod");
-} else {
-  //dev env
-  module.exports = require("./dev");
-}
